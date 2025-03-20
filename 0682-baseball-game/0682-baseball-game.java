2c9f99a30a3d24adecbd9bd4895eb73e26d03294
Muthukumar <kumararumugam825@gmail.com>
2025-03-20 15:27:56 +0530

    Time: 5 ms (10.55%), Space: 42.1 MB (34.35%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -3,20 +3,25 @@ class Solution {
          int sum , sum1 ;
         ArrayList<Integer> list = new ArrayList<>();
         for (String s : str) {
-            if (isNumeric(s)) {
-                list.add(Integer.parseInt(s));
-            } else if (s.contains("C")) {
-                if(!list.isEmpty()){
-                list.removeLast();
-                }
-            } else if (s.contains("+")) {
-               sum= list.get(list.size()-1);
-               int sum2= list.get(list.size()-2);
-                list.add(sum+sum2);
-            } else if (s.contains("D")) {
-                sum1=list.getLast();
-                int sum2 = sum1 * 2;
-                list.add(sum2);
+            switch (s){
+                case "+":
+                    sum= list.get(list.size()-1);
+                    int sum2= list.get(list.size()-2);
+                    list.add(sum+sum2);
+                    break;
+                case "C":
+                    if(!list.isEmpty()){
+                        list.removeLast();
+                    }
+                    break;
+                case "D":
+                    sum1=list.getLast();
+                    int sum3 = sum1 * 2;
+                    list.add(sum3);
+                    break;
+                default:
+                    list.add(Integer.parseInt(s));
+                    break;
             }
 
         }
@@ -29,21 +34,5 @@ class Solution {
         System.out.println(val);
         return val;
     }
-    
-        static boolean isNumeric (String str){
-        boolean number=false;
-        number=isNegative(str);
-        if(Character.isDigit(str.charAt(0)) && number==false) {
-            number = true;
-        }
-
-        return number;
-        }
-        static boolean isNegative(String str){
-        if(str.charAt(0)=='-'){
-            return str.length() >1 && Character.isDigit(str.charAt(1));
-        }
-        return false;
-        }
 
     }
